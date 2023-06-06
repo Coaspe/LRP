a3ee6d6852d3f22a14441b438112663044811710
Coaspe <aspalt85@gmail.com>
2023-06-06 22:34:35 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LRP (Laboratory Resource Planning)
+
+연구실 자재 관리 프로그램
---
+++ android/app/src/main/kotlin/com/example/lrp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lrp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
